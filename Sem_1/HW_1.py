678a096473c5b72f371cd24a0282ca5a9fe5e4ab
Albert Vagapov <va3@list.ru>
2022-11-26 19:14:45 +0500

    Task 1 from HW_1 remaked

--- Sem_1/HW_1.py
+++ Sem_1/HW_1.py
@@ -37,23 +37,20 @@
 
 # 1
 
-# week_day = input("Введите день недели от 1 до 7\n")
-# if week_day == 1:
-#     print("monday")
-# elif week_day == 2:
-#     print("tuesday")
-# elif week_day == 3:
-#     print("wednesday")
-# elif week_day == 4:
-#     print("thursday")
-# elif week_day == 5:
-#     print("friday")
-# elif week_day == 6:
-#     print("saturday")
-# elif week_day == 7:
-#     print("sunday")
+# day_number = input("Введите цифру обозначающую день недели\n")
+
+# if day_number.isdigit():
+
+#     num = int(day_number)
+#     if num > 7 or num < 1:
+#         print("это не цифра обозначающая день недели")
+#     elif num > 5:
+#         print("Да")
+#     else:
+#         print("нет")
 # else:
-#     print("Введите число от 1 до 7")
+#     print("это не цифра обозначающая день недели")
+
 
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
