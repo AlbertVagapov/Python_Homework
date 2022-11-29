5c7adb8b4efd67988f62967541b192701045a2dd
Albert Vagapov <va3@list.ru>
2022-11-29 11:45:59 +0500

    Lectur's tasks added

---
+++ Sem_1/Sem_2.py
@@ -0,0 +1,23 @@
+# colors = ['red', 'green', 'blue']
+# data = open('file.txt', 'a')
+# # data.writelines(colors) 
+# data.write('\nLINE 2\n')
+# data.write('LINE 3\n')
+# data.close()
+
+
+# with open('file.txt', 'a') as data:
+#     data.write('line777\n')
+#     data.write('line888\n')
+
+# path = 'file.txt'
+# data = open(path, 'r')
+# for line in data:
+#     print(line)
+# data.close()
+
+# exit()
+
+# import lib as b
+
+# print(b.f(1))
