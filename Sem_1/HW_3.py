f8d1daec4682d624ef5519d59367dbb2c7f90f9c
Albert Vagapov <va3@list.ru>
2022-12-01 16:13:57 +0500

    Task 3 from HW_3 done

--- Sem_1/HW_3.py
+++ Sem_1/HW_3.py
@@ -37,3 +37,39 @@
 # mult_lst(lst)
 # lst = list(map(int, input("Введите числа через пробел:\n").split()))
 # mult_lst(lst)
+
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+# 3-Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+
+# Пример:
+
+# [1.1, 1.2, 3.1, 5.17, 10.02] => 0.18 или 18
+# [4.07, 5.1, 8.2444, 6.98] - 0.91 или 91
+
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+# 3
+
+# from random import uniform
+
+
+# n = int(input('Введите размер списка '))
+# list1 = []
+# for i in range(n):
+#     f = uniform(0, 9)
+#     list1.append(round(f, 2))
+
+# min = list1[0]
+# max = 0
+# for i in range(len(list1)):
+    
+#     if max < list1[i]:
+#         max = list1[i]
+#     if min > list1[i]:
+#         min = list1[i]
+# dif = (max - int(max)) - (min - int(min))
+
+# print(list1)
+# print(max, min)
+# print(round(dif,2))
