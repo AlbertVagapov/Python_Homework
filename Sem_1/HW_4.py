cba1bfb9a9085efd1c821a07856aa336b9a1f79d
Albert Vagapov <va3@list.ru>
2022-12-06 15:52:25 +0500

    Task 1,2 from HW_4 done

--- Sem_1/HW_4.py
+++ Sem_1/HW_4.py
@@ -30,3 +30,31 @@
 # AAAAAAAAAAAABBBBBBBBBBBCCCCCCCCCCDDDDDDEEEEEFFFFG python is sooooooo coooooool
 # файл второй:
 # 12A11B10C6D5E4FG python is s7o c7ol
+
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+# 1
+
+# num = int(input("Введите число: "))
+# i = 2 # первый простой множитель
+# lst = []
+# old = num
+# while i <= num:
+#     if num % i == 0:
+#         lst.append(i)
+#         num //= i
+#         i = 2
+#     else:
+#         i += 1
+# print(f"Простые множители числа {old} приведены в списке: {lst}")
+
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+# Задача №2: Задайте последовательность чисел. 
+# Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+# lst = list(map(int, input("Введите числа через пробел:\n").split()))
+# print(f"Исходный список: {lst}")
+# new_lst = []
+# [new_lst.append(i) for i in lst if i not in new_lst]
+# print(f"Список из неповторяющихся элементов: {new_lst}")
